319197212ac2b0ece1601aa9c5bab426f5a7b5c4
Romário Lima <devromariolima@gmail.com>
2023-10-03 16:18:48 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.slider').slider();
+    $('.materialboxed').materialbox();
+    $('.modal').modal();
+    $('.parallax').parallax();
+    $('.modal').modal();
+  });
+        
+
+ 
